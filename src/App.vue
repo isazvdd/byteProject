1c7baf7be2f1ca0982c75e8de512d13989fd155d
Isadora Azevedo <isadora.azevedo.700@ufrn.edu.br>
2023-04-19 21:44:42 -0300

    feat: Adicionado a tela Splash (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <MySplash />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MySplash from "@/pages/MySplash.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    MySplash,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  min-height: 100vh;
 }
 </style>
---
+++ src/pages/MySplash.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <img src="@/assets/logo.png" alt="imagem do logo da empresa" class="logo">
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100%;
+  background-color: #1E1E1E;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
